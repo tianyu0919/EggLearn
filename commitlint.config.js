c3f171b1d44c0f8d9de9647a1f63e124f076f1d7
lutianyu <wb_lutianyu@meituan.com>
2024-11-12 18:43:14 +0800

    xxx

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能（feature）
+        'fix', // 修补bug
+        'docs', // 文档（documentation）
+        'style', // 格式（不影响代码运行的变动）
+        'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回滚
+        'build', // 影响构建系统或外部依赖项的更改
+        'ci', // 对CI配置文件和脚本的更改
+      ],
+    ],
+    'subject-case': [0, 'never'],
+  },
+}
